dc9250deeba2c348ad70dfce8720d4fa51bf43bc
Mohamed Ashraf <ma1261327@gmail.com>
2025-05-06 00:26:20 +0300

    edit ClothesDataSource argument

--- src/main/kotlin/data/remote/datasource/clothes/ClothesDataSource.kt
+++ src/main/kotlin/data/remote/datasource/clothes/ClothesDataSource.kt
@@ -3,7 +3,7 @@ package data.remote.datasource.clothes
 import data.remote.responsmodels.clothesModel.ClothesResponseModel
 
 interface ClothesDataSource {
-    fun getCurrentOutfit(weatherCode: String, gender: String): ClothesResponseModel
-    fun getTomorrowOutfit(weatherCode: String, gender: String): ClothesResponseModel
-    fun getWeeklyOutfit(weatherCode: String, gender: String): ClothesResponseModel
+    fun getCurrentOutfit(weatherCode: String): ClothesResponseModel
+    fun getTomorrowOutfit(weatherCode: String): ClothesResponseModel
+    fun getWeeklyOutfit(weatherCode: String): ClothesResponseModel
 }
--- src/main/kotlin/data/remote/datasource/clothes/ClothesRemoteDataSource.kt
+++ src/main/kotlin/data/remote/datasource/clothes/ClothesRemoteDataSource.kt
@@ -3,24 +3,16 @@ package data.remote.datasource.clothes
 import data.remote.responsmodels.clothesModel.ClothesResponseModel
 
 class ClothesRemoteDataSource : ClothesDataSource {
-    override fun getCurrentOutfit(
-        weatherCode: String,
-        gender: String
-    ): ClothesResponseModel {
+    override fun getCurrentOutfit(weatherCode: String): ClothesResponseModel {
         TODO("Not yet implemented")
     }
 
-    override fun getTomorrowOutfit(
-        weatherCode: String,
-        gender: String
-    ): ClothesResponseModel {
+    override fun getTomorrowOutfit(weatherCode: String): ClothesResponseModel {
         TODO("Not yet implemented")
     }
 
-    override fun getWeeklyOutfit(
-        weatherCode: String,
-        gender: String
-    ): ClothesResponseModel {
+    override fun getWeeklyOutfit(weatherCode: String): ClothesResponseModel {
         TODO("Not yet implemented")
     }
+
 }
