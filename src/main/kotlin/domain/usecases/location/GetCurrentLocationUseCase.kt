c19d7e59ed2b45382f9437579756a105c22ff870
Hager Abobakr <hagarabobakr1@gmail.com>
2025-05-06 15:37:30 +0300

    add getCurrentLocation function into GetCurrentLocationUseCase class

--- src/main/kotlin/domain/usecases/location/GetCurrentLocationUseCase.kt
+++ src/main/kotlin/domain/usecases/location/GetCurrentLocationUseCase.kt
@@ -1,5 +1,19 @@
 package domain.usecases.location
 
-class GetCurrentLocationUseCase {
+import data.remote.requestmodels.IpAddressRequestModel
+import domain.models.location.LocationModel
+import domain.repositories.LocationRepository
 
+class GetCurrentLocationUseCase(private val locationRepository: LocationRepository) {
+
+    fun getCurrentLocation(ipAddressRequestModel: IpAddressRequestModel): LocationModel {
+        return LocationModel(
+            ip = "",
+            city = "",
+            countryName = "",
+            latitude = 0.0,
+            longitude = 0.0,
+            region = "",
+        )
+    }
 }
