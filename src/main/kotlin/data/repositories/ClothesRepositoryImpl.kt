1d1e4f38b020d5932c2c298b429b6fbbf18ed4f3
khater <khater@email.com>
2025-05-06 21:17:26 +0300

    update repositories implementations parameters

--- src/main/kotlin/data/repositories/ClothesRepositoryImpl.kt
+++ src/main/kotlin/data/repositories/ClothesRepositoryImpl.kt
@@ -1,16 +1,16 @@
 package data.repositories
 
-import data.remote.datasource.clothes.ClothesRemoteDataSource
+import data.remote.datasource.clothes.ClothesDataSource
 import data.utils.toClothesExceptions
 import domain.models.clothesModels.ClothesModel
 import domain.repositories.ClothesRepository
 
 class ClothesRepositoryImpl(
-    private val clothesRemoteDataSource: ClothesRemoteDataSource
+    private val clothesDataSource: ClothesDataSource
 ) : ClothesRepository {
     override suspend fun getAllOutfit(weatherCode: String): ClothesModel {
         try {
-            val result = clothesRemoteDataSource.getAllOutfit(weatherCode)
+            val result = clothesDataSource.getAllOutfit(weatherCode)
             return result.toClothesModel()
 
         } catch (exception: Exception) {
