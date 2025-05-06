94329bd6a37a442f6780687535ed69c0d40d2a37
khater <khater@email.com>
2025-05-06 19:27:44 +0300

    add functions in domain interfaces for all application

--- src/main/kotlin/domain/repositories/LocationRepository.kt
+++ src/main/kotlin/domain/repositories/LocationRepository.kt
@@ -1,4 +1,8 @@
 package domain.repositories
 
+import domain.models.location.LocationModel
+
 interface LocationRepository {
+    suspend fun getCurrentLocation(ipAddress: String): LocationModel
+
 }
--- src/main/kotlin/domain/repositories/WeatherRepository.kt
+++ src/main/kotlin/domain/repositories/WeatherRepository.kt
@@ -1,4 +1,8 @@
 package domain.repositories
 
+import domain.models.weatherModels.WeatherModel
+
 interface WeatherRepository {
+    suspend fun getWeather(latitude: Double, longitude: Double): WeatherModel
+
 }
