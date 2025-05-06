510ed1a8414e9abdfd8b93e151fc784c421e9eb0
Hager Abobakr <hagarabobakr1@gmail.com>
2025-05-06 20:44:18 +0300

    add unit test setup for GetOutfitUseCaseTest

--- src/test/kotlin/domain/usecases/clothes/GetOutfitUseCaseTest.kt
+++ src/test/kotlin/domain/usecases/clothes/GetOutfitUseCaseTest.kt
@@ -1,4 +1,18 @@
 package domain.usecases.clothes
 
-import org.junit.jupiter.api.Assertions.*
- class GetOutfitUseCaseTest
+import domain.repositories.ClothesRepository
+import io.mockk.mockk
+import org.junit.jupiter.api.BeforeEach
+
+class GetOutfitUseCaseTest{
+  lateinit var getOutfitUseCase: GetOutfitUseCase
+  lateinit var clothesRepository: ClothesRepository
+
+  @BeforeEach
+  fun setUp() {
+   clothesRepository = mockk(relaxed = true)
+   getOutfitUseCase = GetOutfitUseCase(clothesRepository)
+  }
+
+
+ }
