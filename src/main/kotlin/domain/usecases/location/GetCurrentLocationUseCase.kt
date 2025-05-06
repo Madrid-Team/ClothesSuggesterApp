ef8f529bf0927052424ee0475d2b144beb8bc829
Hager Abobakr <hagarabobakr1@gmail.com>
2025-05-06 21:21:36 +0300

    add try catch to the getCurrentLocation function

--- src/main/kotlin/domain/usecases/location/GetCurrentLocationUseCase.kt
+++ src/main/kotlin/domain/usecases/location/GetCurrentLocationUseCase.kt
@@ -3,11 +3,16 @@ package domain.usecases.location
 import data.remote.requestmodels.IpAddressRequestModel
 import domain.models.location.LocationModel
 import domain.repositories.LocationRepository
+import domain.utils.exceptions.LocationExceptions
 
 class GetCurrentLocationUseCase(private val locationRepository: LocationRepository) {
 
     suspend fun getCurrentLocation(ipAddressRequestModel: String): LocationModel {
-        return locationRepository.getCurrentLocation(ipAddressRequestModel)
+       try {
+           return locationRepository.getCurrentLocation(ipAddressRequestModel)
+       } catch (e: LocationExceptions.InvalidIpAddressException){
+           throw e
+       }
 
     }
 }
