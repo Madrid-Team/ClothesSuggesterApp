ea13bf65e73c53ee2ceb51306fd5a9f61a6c758d
Hager Abobakr <hagarabobakr1@gmail.com>
2025-05-06 17:41:25 +0300

    override suspend fun getCurrentLocation

--- src/main/kotlin/data/repositories/LocationRepositoryImpl.kt
+++ src/main/kotlin/data/repositories/LocationRepositoryImpl.kt
@@ -1,6 +1,10 @@
 package data.repositories
 
+import domain.models.location.LocationModel
 import domain.repositories.LocationRepository
 
 class LocationRepositoryImpl : LocationRepository {
+    override suspend fun getCurrentLocation(ipAddressRequestModel: String): LocationModel {
+        TODO("Not yet implemented")
+    }
 }
