a69f4c06d5a8f338a99ff49628d96a93296e6803
Hager Abobakr <hagarabobakr1@gmail.com>
2025-05-06 21:02:38 +0300

    add should throw exception for unknown gender function

--- src/test/kotlin/domain/usecases/clothes/GetOutfitUseCaseTest.kt
+++ src/test/kotlin/domain/usecases/clothes/GetOutfitUseCaseTest.kt
@@ -3,6 +3,7 @@ package domain.usecases.clothes
 import domain.models.clothesModels.ClothesItemModel
 import domain.models.clothesModels.ClothesModel
 import domain.repositories.ClothesRepository
+import domain.utils.exceptions.ClothesExceptions
 import io.mockk.coEvery
 import io.mockk.mockk
 import kotlinx.coroutines.test.TestScope
@@ -10,20 +11,22 @@ import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class GetOutfitUseCaseTest {
     lateinit var getOutfitUseCase: GetOutfitUseCase
     lateinit var clothesRepository: ClothesRepository
+    lateinit var testScope: TestScope
 
     @BeforeEach
     fun setUp() {
         clothesRepository = mockk(relaxed = true)
         getOutfitUseCase = GetOutfitUseCase(clothesRepository)
+        testScope = TestScope()
     }
 
     @Test
     fun `should return male winter clothes for low temperature`() {
-        val testScope = TestScope()
         testScope.runTest {
             val temp = 5.0
             val gender = "male"
@@ -41,19 +44,31 @@ class GetOutfitUseCaseTest {
 
     @Test
     fun `should return female summer clothes for high temperature`() {
-        val testScope = TestScope()
         testScope.runTest {
             val temp = 50.0
             val gender = "female"
             val expected = ClothesModel(
-                maleClothes =  emptyList(),
-                femaleClothes =listOf(ClothesItemModel(id = 1, title = "", description = "summer clothes"))
+                maleClothes = emptyList(),
+                femaleClothes = listOf(ClothesItemModel(id = 1, title = "", description = "summer clothes"))
             )
             coEvery { clothesRepository.getDailyOutfit(temp) } returns expected
 
             val result = getOutfitUseCase.getDailyOutfit(temp, gender)
 
             assertEquals(expected, result)
+        }
+    }
+
+    @Test
+    fun `should throw exception for unknown gender`() {
+        testScope.runTest {
+            val temp = 20.0
+            val gender = "alien"
+            coEvery { clothesRepository.getDailyOutfit(temp) } throws ClothesExceptions.UnknownGenderException()
+
+            assertThrows<ClothesExceptions.UnknownGenderException> {
+                getOutfitUseCase.getDailyOutfit(temp, gender)
+            }
         }
     }
 
