7e448df7173056dfbbacda847bf67873f6341271
abdelrahman <abdelrahmankarim7@gmail.com>
2025-05-06 23:33:54 +0300

    add weather usecase implementation

--- src/test/kotlin/domain/usecases/weather/GetCurrentWeatherUseCaseTest.kt
+++ src/test/kotlin/domain/usecases/weather/GetCurrentWeatherUseCaseTest.kt
@@ -11,6 +11,8 @@ import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.every
 import io.mockk.mockk
+import kotlinx.coroutines.test.TestScope
+import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.assertThrows
@@ -19,74 +21,74 @@ import kotlin.test.Test
 class GetCurrentWeatherUseCaseTest {
     lateinit var weatherRepository: WeatherRepository
     lateinit var getCurrentWeatherUseCase: GetCurrentWeatherUseCase
+    lateinit var testScope: TestScope
 
     @BeforeEach
     fun setup(){
         weatherRepository = mockk(relaxed = true)
         getCurrentWeatherUseCase = GetCurrentWeatherUseCase(weatherRepository)
+        testScope = TestScope()
 
     }
 
     @Test
     fun `get current weather should return current weather model when latitude and longitude are valid `(){
-        //Given
-        val lat = 41.2
-        val lng = 62.3
-        val expectedWeatherModel = createWeatherModel()
+        testScope.runTest {
+            //Given
+            val lat = 41.2
+            val lng = 62.3
+            val expectedWeatherModel = createWeatherModel()
 
 
 
-        coEvery {  weatherRepository.getWeather(lat,lng) } returns expectedWeatherModel
+            coEvery { weatherRepository.getWeather(lat, lng) } returns expectedWeatherModel
 
-        //when
-        val result = getCurrentWeatherUseCase.getCurrentWeather(lat,lng)
+            //when
+            val result = getCurrentWeatherUseCase.getCurrentWeather(lat, lng)
 
-        //then
-        assertEquals(expectedWeatherModel.current,result)
+            //then
+            assertEquals(expectedWeatherModel.current, result)
 
-
+        }
     }
 
 
     @Test
     fun `get current weather should throw exception when lat  is invalid`(){
-        //Given
-        val lat = 112.5
-        val lng = 62.3
-        val expectedWeatherModel = createWeatherModel()
+        testScope.runTest {
+            //Given
+            val lat = 112.5
+            val lng = 62.3
+
+            coEvery { weatherRepository.getWeather(lat, lng) } throws WeatherExceptions.WeatherDataOutOfRangeException()
+
+            //when & then
+            assertThrows<WeatherExceptions.WeatherDataOutOfRangeException> {
+                getCurrentWeatherUseCase.getCurrentWeather(lat, lng)
+            }
 
 
-
-        coEvery {  weatherRepository.getWeather(lat,lng) } returns expectedWeatherModel
-
-        //when & then
-       assertThrows<WeatherExceptions.WeatherDataOutOfRangeException>{
-           getCurrentWeatherUseCase.getCurrentWeather(lat,lng)}
-
-
-
+        }
 
 
     }
 
     @Test
     fun `get current weather should throw exception when lng  is invalid`(){
-        //Given
-        val lat = 80.1
-        val lng = 192.1
-        val expectedWeatherModel = createWeatherModel()
+        testScope.runTest {
+            //Given
+            val lat = 80.1
+            val lng = 192.1
+
+            coEvery { weatherRepository.getWeather(lat, lng) } throws WeatherExceptions.WeatherDataOutOfRangeException()
+
+            //when & then
+            assertThrows<WeatherExceptions.WeatherDataOutOfRangeException> {
+                getCurrentWeatherUseCase.getCurrentWeather(lat, lng)
+            }
 
 
-
-        coEvery {  weatherRepository.getWeather(lat,lng) } returns expectedWeatherModel
-
-        //when & then
-        assertThrows<WeatherExceptions.WeatherDataOutOfRangeException>{
-            getCurrentWeatherUseCase.getCurrentWeather(lat,lng)}
-
-
-
-
+        }
 
     }
 
