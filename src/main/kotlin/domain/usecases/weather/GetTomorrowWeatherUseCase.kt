0b683864e0551073937571d23bd6e8eea8d637d8
abdelrahman <abdelrahmankarim7@gmail.com>
2025-05-06 23:35:55 +0300

    add get tomorrow weather usecase implementation

--- src/main/kotlin/domain/usecases/weather/GetTomorrowWeatherUseCase.kt
+++ src/main/kotlin/domain/usecases/weather/GetTomorrowWeatherUseCase.kt
@@ -3,19 +3,18 @@ package domain.usecases.weather
 import domain.models.weatherModels.CurrentWeatherModel
 import domain.models.weatherModels.DailyWeatherTemperatureModel
 import domain.repositories.WeatherRepository
+import domain.utils.exceptions.WeatherExceptions
 import kotlin.Double
 import kotlin.collections.List
 
 class GetTomorrowWeatherUseCase(private val weatherRepository: WeatherRepository) {
 
    suspend fun getTomorrowWeatherUseCase(latitude: Double, longitude: Double): DailyWeatherTemperatureModel {
-        return DailyWeatherTemperatureModel(
-            temperatureMax = listOf(),
-            temperatureMin = listOf(),
-            time = listOf(),
-            weatherCode = listOf()
-        )
-
+       try {
+           return weatherRepository.getWeather(latitude,longitude).daily
+       }catch (e: WeatherExceptions){
+           throw e
+       }
 
     }
 
