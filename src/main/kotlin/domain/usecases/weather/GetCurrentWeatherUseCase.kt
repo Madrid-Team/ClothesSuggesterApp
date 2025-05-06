c8fc90cb009d02e0488a866650211c18c3d1e4fb
abdelrahman <abdelrahmankarim7@gmail.com>
2025-05-06 17:49:22 +0300

    add get current weather usecase

--- src/main/kotlin/domain/usecases/weather/GetCurrentWeatherUseCase.kt
+++ src/main/kotlin/domain/usecases/weather/GetCurrentWeatherUseCase.kt
@@ -1,4 +1,30 @@
 package domain.usecases.weather
 
-class GetCurrentWeatherUseCase {
+import data.remote.requestmodels.IpAddressRequestModel
+import domain.models.location.LocationModel
+import domain.models.weatherModels.CurrentWeatherModel
+import domain.repositories.WeatherRepository
+import kotlin.Int
+
+class GetCurrentWeatherUseCase(private val weatherRepository: WeatherRepository) {
+
+    fun getCurrentWeather(latitude: Double, longitude : Double): CurrentWeatherModel {
+        return CurrentWeatherModel(
+            cloudCover = 0,
+            interval = 0,
+            isDay = 0,
+            precipitation = 0.0,
+            rain = 0.0,
+            relativeHumidity = 0,
+            showers = 0.0,
+            snowfall = 0.0,
+            temperature = 0.0,
+            time = "",
+            weatherCode = 0,
+            windSpeed = 0.0
+        )
+
+
+    }
+
 }
