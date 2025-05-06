f9d4943a9a21b233b2d44b55a56b83326db8d24d
Hager Abobakr <hagarabobakr1@gmail.com>
2025-05-06 21:12:28 +0300

    add try catch to the getDailyOutfit function

--- src/main/kotlin/domain/usecases/clothes/GetOutfitUseCase.kt
+++ src/main/kotlin/domain/usecases/clothes/GetOutfitUseCase.kt
@@ -1,25 +1,28 @@
 package domain.usecases.clothes
 
-import domain.models.clothesModels.ClothesItemModel
 import domain.models.clothesModels.ClothesModel
 import domain.repositories.ClothesRepository
 import domain.utils.exceptions.ClothesExceptions
 
 class GetOutfitUseCase(private val clothesRepository: ClothesRepository) {
     suspend fun getDailyOutfit(temperature: Double, gender: String): ClothesModel {
-        val outfit = clothesRepository.getDailyOutfit(temperature)
-        return when (gender) {
-            "male" -> ClothesModel(
-                maleClothes = outfit.maleClothes,
-                femaleClothes = emptyList()
-            )
+        return try {
+            val outfit = clothesRepository.getDailyOutfit(temperature)
+            when (gender.lowercase()) {
+                "male" -> ClothesModel(
+                    maleClothes = outfit.maleClothes,
+                    femaleClothes = emptyList()
+                )
 
-            "female" -> ClothesModel(
-                maleClothes = emptyList(),
-                femaleClothes = outfit.femaleClothes
-            )
+                "female" -> ClothesModel(
+                    maleClothes = emptyList(),
+                    femaleClothes = outfit.femaleClothes
+                )
 
-            else -> throw ClothesExceptions.UnknownGenderException()
+                else -> throw ClothesExceptions.UnknownGenderException()
+            }
+        } catch (e: ClothesExceptions.UnknownGenderException) {
+            throw e
         }
 
 
