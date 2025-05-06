6328d561e1853d4055d86656c7903fae0516f601
abdelrahman <abdelrahmankarim7@gmail.com>
2025-05-06 17:48:55 +0300

    add get weather function to repository

--- src/main/kotlin/domain/repositories/WeatherRepository.kt
+++ src/main/kotlin/domain/repositories/WeatherRepository.kt
@@ -1,4 +1,9 @@
 package domain.repositories
 
+
+import domain.models.weatherModels.WeatherModel
+
 interface WeatherRepository {
+    suspend fun getWeather(latitude: Double, longitude : Double ) : WeatherModel
+
 }
