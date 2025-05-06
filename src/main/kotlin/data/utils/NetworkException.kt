f7320c4a334489ea766fdce0c0ab3793707fee76
Mohamed Ashraf <ma1261327@gmail.com>
2025-05-06 18:10:42 +0300

    add NoInternetConnectionException in  NetworkException class

--- src/main/kotlin/data/utils/NetworkException.kt
+++ src/main/kotlin/data/utils/NetworkException.kt
@@ -1,6 +1,7 @@
 package data.utils
 
 open class NetworkException(message: String? = null, cause: Throwable? = null): Exception(message) {
+    class NoInternetConnectionException(cause: Throwable? = null) : NetworkException("No internet connection", cause)
     class SerializationException(cause: Throwable? = null) : NetworkException("Failed to serialize response", cause)
     class RequestTimeoutException : NetworkException("Request timed out")
     class TooManyRequestsException : NetworkException("Too many requests")
