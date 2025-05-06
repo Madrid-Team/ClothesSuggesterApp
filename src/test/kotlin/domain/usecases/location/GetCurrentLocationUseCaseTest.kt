898550762626ff88102f3cea05fba5b2a34dcf60
Hager Abobakr <hagarabobakr1@gmail.com>
2025-05-06 15:48:13 +0300

    add unit test setup for GetCurrentLocationUseCase

--- src/test/kotlin/domain/usecases/location/GetCurrentLocationUseCaseTest.kt
+++ src/test/kotlin/domain/usecases/location/GetCurrentLocationUseCaseTest.kt
@@ -1,4 +1,19 @@
 package domain.usecases.location
 
+import domain.repositories.LocationRepository
 import org.junit.jupiter.api.Assertions.*
- class GetCurrentLocationUseCaseTest
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+ class GetCurrentLocationUseCaseTest{
+  lateinit var getCurrentLocationUseCase: GetCurrentLocationUseCase
+  lateinit var locationRepository: LocationRepository
+  @BeforeEach
+  fun setUp() {
+   locationRepository = mockk(relaxed = true)
+   getCurrentLocationUseCase = GetCurrentLocationUseCase(locationRepository)
+  }
+
+ }
