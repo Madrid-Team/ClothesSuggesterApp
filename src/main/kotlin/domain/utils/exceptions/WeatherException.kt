de9fe8667305fcbc4e2f3dd721d8933ff0caf21d
Mohamed Ashraf <ma1261327@gmail.com>
2025-05-07 12:53:05 +0300

    edit exceptions in WeatherException

--- src/main/kotlin/domain/utils/exceptions/WeatherException.kt
+++ src/main/kotlin/domain/utils/exceptions/WeatherException.kt
@@ -1,11 +1,7 @@
 package domain.utils.exceptions
 
 open class WeatherException(message: String) : ClothesSuggesterExceptions(message) {
-    class ForecastRangeException :
-        WeatherException("The requested date is out of the supported weather forecast range.")
-
-    class UnsupportedWeatherConditionException :
-        WeatherException("The current weather conditions are not supported for outfit suggestions.")
-
-    class WeatherDataException(message: String) : WeatherException(message)
+    class InvalidWeatherDataException(message: String) : WeatherException(message)
+    class WeatherNetworkException(message: String) : WeatherException(message)
+    class WeatherApiException(message: String) : WeatherException(message)
 }
