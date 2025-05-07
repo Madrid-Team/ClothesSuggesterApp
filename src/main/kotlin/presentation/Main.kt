cb83ce93e2fccd8356d0c2ccecacf5bd78ea1a53
Hager Abobakr <hagarabobakr1@gmail.com>
2025-05-07 21:08:06 +0300

    add main

--- src/main/kotlin/presentation/Main.kt
+++ src/main/kotlin/presentation/Main.kt
@@ -6,7 +6,10 @@ import di.modules.networkModule
 import di.modules.repositoryModule
 import di.modules.useCasesModule
 import org.koin.core.context.GlobalContext.startKoin
+import org.koin.java.KoinJavaComponent.getKoin
 
-fun main() {
+suspend fun main() {
     startKoin { modules(dataSourceModule,networkModule, repositoryModule, useCasesModule, cliModule) }
+    val clothesSuggesterCLI: ClothesSuggesterCLI= getKoin().get()
+    clothesSuggesterCLI.start()
 }
