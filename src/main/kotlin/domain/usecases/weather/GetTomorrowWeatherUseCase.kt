5b81eb24eb142495d1128329ce755b665537bb08
abdelrahman <abdelrahmankarim7@gmail.com>
2025-05-07 06:19:34 +0300

    add dummy get weather function in get weather usecase for testing

--- src/main/kotlin/domain/usecases/weather/GetTomorrowWeatherUseCase.kt
+++ src/main/kotlin/domain/usecases/weather/GetTomorrowWeatherUseCase.kt
@@ -1,4 +1,10 @@
 package domain.usecases.weather
 
-class GetTomorrowWeatherUseCase {
+import domain.repositories.WeatherRepository
+
+class GetTomorrowWeatherUseCase(private val weatherRepository: WeatherRepository) {
+    suspend fun getTomorrowWeather(latitude : Double, longitude : Double){
+
+    }
+
 }
