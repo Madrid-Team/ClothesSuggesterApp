ff4b4c6ef0bcf3039147e217b854edfcdc2779bb
Hager Abobakr <hagarabobakr1@gmail.com>
2025-05-07 08:53:10 +0300

    add getWeeklyOutfit function into GetWeeklyOutfitUseCase class

--- src/main/kotlin/domain/usecases/clothes/GetWeeklyOutfitUseCase.kt
+++ src/main/kotlin/domain/usecases/clothes/GetWeeklyOutfitUseCase.kt
@@ -1,6 +1,10 @@
 package domain.usecases.clothes
 
+import domain.models.clothesModels.ClothesItemModel
 import domain.repositories.ClothesRepository
 
 class GetWeeklyOutfitUseCase(private val clothesRepository: ClothesRepository) {
+    suspend fun getWeeklyOutfit(tempList: List<String>, gender: String): List<List<ClothesItemModel>>{
+        return emptyList()
+    }
 }
