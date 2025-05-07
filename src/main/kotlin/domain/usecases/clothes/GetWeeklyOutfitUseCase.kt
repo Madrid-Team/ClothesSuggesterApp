34582223dabf6b076e06107edd393d2443f683a3
Hager Abobakr <hagarabobakr1@gmail.com>
2025-05-07 09:48:30 +0300

    implement getWeeklyOutfit function

--- src/main/kotlin/domain/usecases/clothes/GetWeeklyOutfitUseCase.kt
+++ src/main/kotlin/domain/usecases/clothes/GetWeeklyOutfitUseCase.kt
@@ -2,9 +2,23 @@ package domain.usecases.clothes
 
 import domain.models.clothesModels.ClothesItemModel
 import domain.repositories.ClothesRepository
+import domain.utils.Gender
+import domain.utils.exceptions.ClothesExceptions
 
 class GetWeeklyOutfitUseCase(private val clothesRepository: ClothesRepository) {
     suspend fun getWeeklyOutfit(tempList: List<String>, gender: String): List<List<ClothesItemModel>>{
-        return emptyList()
+        val genderEnum = try {
+            Gender.fromString(gender)
+        } catch (e: Exception) {
+            throw ClothesExceptions.UnknownGenderException()
+        }
+
+        return tempList.map { temp ->
+            val outfit = clothesRepository.getAllOutfit(temp)
+            when (genderEnum) {
+                Gender.MALE -> outfit.maleClothes
+                Gender.FEMALE -> outfit.femaleClothes
+            }
+        }
     }
 }
