4e365d85ddf87a4bfcfceb171f7369f96c77d015
khater <khater@email.com>
2025-05-07 11:55:49 +0300

    update gender from string into enum class

--- src/main/kotlin/domain/usecases/clothes/GetOutfitUseCase.kt
+++ src/main/kotlin/domain/usecases/clothes/GetOutfitUseCase.kt
@@ -6,10 +6,10 @@ import domain.utils.Gender
 import domain.utils.exceptions.ClothesException
 
 class GetOutfitUseCase(private val clothesRepository: ClothesRepository) {
-    suspend fun getDailyOutfit(temperature: String, gender: String): List<ClothesItemModel> {
+    suspend fun getDailyOutfit(temperature: String, gender: Gender): List<ClothesItemModel> {
         return try {
             val outfit = clothesRepository.getAllOutfit(temperature)
-             when (Gender.fromString(gender)) {
+             when (gender) {
                 Gender.MALE -> outfit.maleClothes
                 Gender.FEMALE -> outfit.femaleClothes
             }
--- src/main/kotlin/domain/utils/Gender.kt
+++ src/main/kotlin/domain/utils/Gender.kt
@@ -1,20 +1,6 @@
 package domain.utils
 
-import domain.utils.exceptions.ClothesException
-
 enum class Gender {
     MALE,
     FEMALE;
-
-    companion object {
-        fun fromString(value: String): Gender {
-
-            return when (value.lowercase()) {
-                "male" -> MALE
-                "female" -> FEMALE
-                else -> throw ClothesException.UnknownGenderException("Unknown gender '$value'")
-            }
-
-        }
-    }
 }
