cd64976e93658ca1ce8e12d11765b966bdeca725
Mohamed Ashraf <ma1261327@gmail.com>
2025-05-07 12:57:50 +0300

    edit exceptions in ClothesException

--- src/main/kotlin/domain/utils/exceptions/ClothesException.kt
+++ src/main/kotlin/domain/utils/exceptions/ClothesException.kt
@@ -2,13 +2,7 @@ package domain.utils.exceptions
 
 
 open class ClothesException(message: String) : ClothesSuggesterExceptions(message) {
-    class UnsupportedTemperatureRangeException :
-        ClothesException("The temperature is outside the supported range for outfit recommendations.")
-
-    class ClothingDataException(message: String) : ClothesException(message)
-
-    class UnknownGenderException(message: String) : ClothesException(message)
-
-    class OutfitNotFoundException :
-        ClothesException("Outfit Not Found Exception")
+    class InvalidClothesDataException(message: String) : ClothesException(message)
+    class ClothesNetworkException(message: String) : ClothesException(message)
+    class ClothesApiException(message: String) : ClothesException(message)
 }
