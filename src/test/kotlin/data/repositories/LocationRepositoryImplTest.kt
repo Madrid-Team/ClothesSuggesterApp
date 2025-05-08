8fafaecc71fb0b7399fe15d3f4ccba32d4688ec6
Hager Abobakr <hagarabobakr1@gmail.com>
2025-05-08 19:10:08 +0300

    add getCurrentLocation returns correct LocationModel when datasource succeeds to LocationRepositoryImplTest

--- src/test/kotlin/data/repositories/LocationRepositoryImplTest.kt
+++ src/test/kotlin/data/repositories/LocationRepositoryImplTest.kt
@@ -1,8 +1,14 @@
 package data.repositories
 
 import data.remote.datasource.location.LocationDataSource
+import data.remote.responsmodels.locationModel.LocationResponseModel
+import domain.models.locationModels.LocationModel
+import io.mockk.coEvery
 import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 
 class LocationRepositoryImplTest{
   private lateinit var locationDataSource: LocationDataSource
@@ -15,5 +21,33 @@ class LocationRepositoryImplTest{
        locationDataSource
    )
   }
+    @Test
+    fun `getCurrentLocation returns correct LocationModel when datasource succeeds`() = runTest {
+        // Given
+        val fakeResponse = createLocationResponseModel(
+            ip = "192.168.1.1",
+            city = "Cairo",
+            countryName = "Egypt",
+            latitude = 30.0444,
+            longitude = 31.2357,
+            region = "Cairo"
+        )
+        coEvery { locationDataSource.getCurrentLocation("192.168.1.1") } returns fakeResponse
+
+        // When
+        val result = repository.getCurrentLocation("192.168.1.1")
+
+        // Then
+        val expected = LocationModel(
+            ip = "192.168.1.1",
+            city = "Cairo",
+            countryName = "Egypt",
+            latitude = 30.0444,
+            longitude = 31.2357,
+            region = "Cairo"
+        )
+
+        assertEquals(expected, result)
+    }
 
 }
