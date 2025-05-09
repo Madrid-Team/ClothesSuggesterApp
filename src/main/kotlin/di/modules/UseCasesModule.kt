ff27e75dbb37f052fee0f7eab945644d72c4a65c
khater <khater@email.com>
2025-05-09 16:03:51 +0300

    update app modules

--- src/main/kotlin/di/modules/UseCasesModule.kt
+++ src/main/kotlin/di/modules/UseCasesModule.kt
@@ -3,21 +3,15 @@ package di.modules
 
 import domain.usecases.clothes.GetOutfitUseCase
 import domain.usecases.clothes.GetWeeklyOutfitUseCase
-import domain.usecases.location.GetCurrentLocationUseCase
-import domain.usecases.location.GetIpAddressUseCase
 import domain.usecases.weather.GetCurrentWeatherUseCase
-import domain.usecases.weather.GetTomorrowWeatherUseCase
 import domain.usecases.weather.GetWeeklyWeatherUseCase
 import org.koin.dsl.module
 
 val useCasesModule = module {
     single { GetOutfitUseCase(get()) }
     single { GetWeeklyOutfitUseCase(get()) }
-    single { GetCurrentLocationUseCase(get()) }
-    single { GetWeeklyWeatherUseCase(get()) }
+     single { GetWeeklyWeatherUseCase(get()) }
     single { GetCurrentWeatherUseCase(get()) }
-    single { GetTomorrowWeatherUseCase(get()) }
-    single { GetIpAddressUseCase(get()) }
 
 }
 
